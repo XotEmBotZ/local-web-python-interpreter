0aedd2a5bd17e76e52ad877d65f652919e5b402e
XotEmBotZ <xotembotzop@gmail.com>
2024-07-03 06:52:22 +0530

    v2

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,49 @@
-var name = 'simple nodexperts cache v2',
-    filesToCache = [
-        './index.html',
-        'https://pyscript.net/alpha/pyscript.js',
-        'https://pyscript.net/alpha/pyscript.css',
-        'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide_py.tar',
-        'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/packages.json',
-        'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js',
-        'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.asm.js',
-        'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/micropip-0.1-py3-none-any.whl',
-        'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyparsing-3.0.7-py3-none-any.whl',
-        'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/packaging-21.3-py3-none-any.whl',
-        'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/distutils.tar',
-        './index.js',
-        './service-worker.js',
-        // './public/icon512_maskable.png.png',
-        // './public/icon512_rounded.png',
-        // './public/manifest.json',
-    ];
+// service-worker.js
 
-// event.waitUntil will stop the flow till the Promise is resolved
+const putInCache = async (request, response) => {
+    const cache = await caches.open("v1");
+    await cache.put(request, response);
+};
 
-self.addEventListener('install', function (event) {
-    console.log("installing");
-    event.waitUntil(
-        // opens cache
-        // cache is an object which is available inside service-worker
-        caches.open(name).then(function (cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
-});
+const cacheFirst = async ({ request, fallbackUrl }) => {
+    // First try to get the resource from the cache.
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
 
-self.addEventListener('activate', function (event) {
-    console.log("activating");
-    event.waitUntil(
-        caches.keys().then(function (cachedFiles) {
-            return Promise.all(cachedFiles.map(function (cacheFile) {
-                // everytime a cache version changes, old files are removed from cache
-                if (cacheFile !== name) {
-                    console.log('Removing Cached Files from Cache - ', cacheFile);
-                    return caches.delete(cacheFile);
-                }
-            }));
-        })
-    );
-});
+    // If the response was not found in the cache,
+    // try to get the resource from the network.
+    try {
+        const responseFromNetwork = await fetch(request);
+        // If the network request succeeded, clone the response:
+        // - put one copy in the cache, for the next time
+        // - return the original to the app
+        // Cloning is needed because a response can only be consumed once.
+        putInCache(request, responseFromNetwork.clone());
+        return responseFromNetwork;
+    } catch (error) {
+        // If the network request failed,
+        // get the fallback response from the cache.
+        const fallbackResponse = await caches.match(fallbackUrl);
+        if (fallbackResponse) {
+            return fallbackResponse;
+        }
+        // When even the fallback response is not available,
+        // there is nothing we can do, but we must always
+        // return a Response object.
+        return new Response("Network error happened", {
+            status: 408,
+            headers: { "Content-Type": "text/plain" },
+        });
+    }
+};
 
-
-self.addEventListener('fetch', function (event) {
-    console.log("fetching");
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-        fetch(event.request).catch(function () {
-            return caches.match(event.request);
-        })
+        cacheFirst({
+            request: event.request,
+            fallbackUrl: "/fallback.html",
+        }),
     );
 });
